b944bf358964a1bf4846a9d30e9190d78bcdc5bf
Sam Oberg <samueloberg@hotmail.com>
2021-06-24 19:10:39 -0700

    projectCard styling

--- my_portfolio/src/components/ProjectCard/index.js
+++ my_portfolio/src/components/ProjectCard/index.js
@@ -5,8 +5,8 @@ function ProjectCard(props) {
 
     return (
         <div className="project">
-            <h2>{props.name}</h2>
-            <img src={props.image} width="500" height="300" alt="projectImg"/>
+            <h2 className="pcHeader">{props.name}</h2>
+            <img className="pcImage" src={props.image} alt="projectImg"/>
             <p>{props.description}</p>
             <a className="link" href={props.link}>Deploy</a>
         </div>

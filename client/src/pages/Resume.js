224d6db6a19562af43ca949c7409b7e9b2115d2b
Sam Oberg <samueloberg@hotmail.com>
2021-07-26 18:52:30 -0700

    mobile design for project and resume

--- client/src/pages/Resume.js
+++ client/src/pages/Resume.js
@@ -2,11 +2,11 @@ import React from "react";
 
 function Resume() {
   return (
-    <>
+    <section className='resumeDiv'>
          <h3>Download My Resume!</h3>
-            <img src="./images/resume.PNG" alt="myresume"/>
+            <img className='resumeImage' src="./images/resume.PNG" alt="myresume"/>
                 <a href="./documents/Resume.pdf">Download</a>
-    </>
+    </section>
   );
 }
 

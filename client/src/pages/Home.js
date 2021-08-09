e93da6710e3b9b73e8cc37d73de051f6f327b70c
Sam Oberg <samueloberg@hotmail.com>
2021-08-09 06:51:53 -0700

    home page work and tech aside creation

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,29 +6,31 @@ function Home() {
     <div className="homeGrid">
         <section>
             <h2 className='homeHeader'>About Me</h2>
-                <p className="bio">
-                    An aspiring Software Developer living and working in the city of Seattle as 
-                    a Corporate Buyer for a supply company and looking to 
-                    change careers.
-                </p>
-                <p className="bio">
-                    Took the leap and joined a full-stack 
-                    coding bootcamp hosted by the University of Washington this 
-                    past January. I am very excited to see where this experience 
-                    will take me.
-                </p>
-                <p className="bio">
-                    The main language that we have been learning is JavaScript, 
-                    and I have just started learning Node.js, which I am looking 
-                    forward to mastering soon. By the end of the program, we will 
-                    progress to studying REACT and several other programs centered
-                     around JS.
-                </p>
+                <div className='bioDiv'>
+                    <p className="bio">
+                        An aspiring Software Developer living and working in the city of Seattle as 
+                        a Corporate Buyer for a supply company and looking to 
+                        change careers.
+                    </p>
+                    <p className="bio">
+                        Took the leap and joined a full-stack 
+                        coding bootcamp hosted by the University of Washington this 
+                        past January. I am very excited to see where this experience 
+                        will take me.
+                    </p>
+                    <p className="bio">
+                        The main language that we have been learning is JavaScript, 
+                        and I have just started learning Node.js, which I am looking 
+                        forward to mastering soon. By the end of the program, we will 
+                        progress to studying REACT and several other programs centered
+                        around JS.
+                    </p>
+                </div>
         </section>
-        <div className='imgTechGrid'>
             <img className="profilePicture" src="./images/me.jpg" alt="projectImg"/>
-            <TechnologyCard />
-        </div>
+            <div class="techAside">
+                <TechnologyCard />
+            </div>
     </div>
     
   );

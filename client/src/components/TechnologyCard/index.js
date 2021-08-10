a7e241ad7c36fc2b1b46cc1fefeea41422dc7e57
Sam Oberg <samueloberg@hotmail.com>
2021-08-10 06:58:16 -0700

    tech experiance styling work

--- client/src/components/TechnologyCard/index.js
+++ client/src/components/TechnologyCard/index.js
@@ -4,15 +4,44 @@ import './style.css'
 function TechnologyCard() {
 
     return (
-        <>
+        <section>
             <h3 className='techDivHeader'>Technological Experience:</h3>
-                <h4 className="experienceHeader">Front-End Development</h4>
-                    <h5>Project Example Name</h5>
+                <h4 className='experienceHeader'>Front-End Development</h4>
+                    <p className='techExperience'>
+                        I have had extensive experience working on front-end projects 
+                        in groups settings and on solo projects. Through using HTML, CSS, 
+                        JavaScript and React.js, I am able to create interactive and clean 
+                        UI's.
+                    </p>
+                    <ul className='projectList'>
+                        <a href='https://github.com/sosoberg/ecommerce_backend'>
+                            <li>Worth A Shot (Group Project)</li>
+                        </a>
+                    </ul>
                 <h4 className="experienceHeader">Back-End Development</h4>
-                    <h5>Project Example Name</h5>
+                    <p className='techExperience'>
+                        I have a list of projects that are focused on the back-end development 
+                        such as creating and connecting to databases with MongoDB and MySQL 
+                        using Mongoose.js and Sequelize.js.
+                    </p>
+                    <ul className='projectList'>
+                        <a href='https://github.com/sosoberg/ecommerce_backend'>
+                            <li>E-Commerce Back-End Project</li>
+                        </a>
+                    </ul>
                 <h4 className="experienceHeader">Full Stack</h4>
-                    <h5>Project Example Name</h5>
-        </>
+                    <p className='techExperience'>
+                        I have created many full stack applications on my own and in group settings. 
+                        Maintaining API routes, managing databases, and creating an immersive interface 
+                        experience for the user.
+                    </p>
+                    <ul className='projectList'>
+                        <a href='https://github.com/sosoberg/B-There'>
+                            <li>B-There! (Group Project)</li>
+                        </a>
+                    </ul>
+                <div className='emptyspace'></div>
+        </section>
     );
 };
 
